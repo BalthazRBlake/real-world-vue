78313667484d132ccb4c5355b6988ad3e1ba19e0
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-21 18:37:21 +0300

    redirecting router with alias

--- src/router/index.js
+++ src/router/index.js
@@ -13,15 +13,12 @@ const routes = [
   {
     path: "/about-us",
     name: "About",
+    alis: "/about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
-    path: "/about",
-    redirec: { name: "About" } // redirec: "/about-us" // if not using named router
   }
 ];
 
