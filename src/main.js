d62adba6527eeb9a500d0a468710f38ed3d1335d
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-23 23:26:49 +0300

    Single Global component

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseIcon from "@/components/BaseIcon.vue";
 
+Vue.component("BaseIcon", BaseIcon); //Register the global component ('Name for the App', Imported Component Name)
+//It should be declared before new Vue()
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount("#app");
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,15 +2,16 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard />
+    <BaseIcon />
   </div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard.vue';
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   components: {
-    EventCard,
-  },
+    EventCard
+  }
 };
 </script>
