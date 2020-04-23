c0cfb44f24c85dd246091d063dce5ef2243a6622
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-23 21:25:53 +0300

    Nested single component | scoped Styles

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h4>{{ title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Beach Cleanup"
+    };
+  }
+};
+</script>
+<!--scoped makes style to be applied just to this component-->
+<style scoped>
+h4 {
+  color: green;
+}
+</style>
