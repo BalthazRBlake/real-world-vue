677673fc471ca9737b6da6d7a396d28dbf2d7d7f
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-25 18:28:03 +0300

    2nd API call from EventService for EventShow

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,8 +1,28 @@
 <template>
-  <h1>Showing event #{{ id }}</h1>
+  <div>
+    <h1>Showing event {{ event.title }}</h1>
+    <h2>Showing event {{ event.id }}</h2>
+  </div>
 </template>
+
 <script>
+import EventService from "@/services/EventService.js";
+
 export default {
-  props: ['id'],
+  props: ["id"],
+  data() {
+    return {
+      event: {}
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }
 };
 </script>
