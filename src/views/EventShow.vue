6a8ad41c06c3910d1c399f92fc3992cd555726ad
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-25 18:50:21 +0300

    event show template

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,7 +1,34 @@
 <template>
   <div>
-    <h1>Showing event {{ event.title }}</h1>
-    <h2>Showing event {{ event.id }}</h2>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+
+    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+
+    <address>{{ event.location }}</address>
+
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -26,3 +53,24 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
