40e6788ebe1e6783d8fe906b23a9889b68843fa6
FHHF <fabianhumbertohernandez@gmail.com>
2020-05-07 18:59:11 +0300

    importing state from store & creating computed props from it

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an event, {{ userName }}</h1>
+    <p>Event created by with id {{ userID }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name;
+    },
+    userID() {
+      return this.$store.state.user.id;
+    }
+  }
+};
+</script>
+
+<style></style>
