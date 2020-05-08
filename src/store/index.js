d79f140bc8ae42f35f3e6d962fa7e3a6a077ecf7
FHHF <fabianhumbertohernandez@gmail.com>
2020-05-08 21:16:51 +0300

    moved EventService to Store | fetching data and dipatching with Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -19,15 +19,29 @@ export default new Vuex.Store({
     events: []
   },
   mutations: {
+    // MUTATION_NAME(state, payload)
     ADD_EVENT(state, event) {
       state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+      state.events = events;
     }
   },
   actions: {
+    // methodName( { contextObject }, ?payload )
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event);
       });
+    },
+    fetchEvents({ commit }) { 
+      EventService.getEvents()
+        .then(response => {
+          commit("SET_EVENTS", response.data);
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
     }
   },
   getters: {
